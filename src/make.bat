b5f61490fb5747330b6790121ee94090ab3bedcb
Robert Spencer <robert.spencer94@gmail.com>
2013-08-22 16:46:34 +0200

    Added make and updated gitignore accordingly

---
+++ src/make.bat
@@ -0,0 +1,9 @@
+g++ -ggdb main.cpp -c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -I include -o main.o
+g++ -ggdb game.cpp -c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -I include -o game.o
+g++ -ggdb graphics.cpp -c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -I include -o graphics.o
+g++ -ggdb heightmap.cpp -c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -I include -o heightmap.o
+g++ -ggdb vector3.cpp -c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -I include -o vector3.o
+g++ -ggdb camera.cpp -c -I"C:\Program Files\Common Files\MinGW\freeglut\include" -I include -o camera.o
+
+
+g++  -o a.exe main.o graphics.o game.o camera.o heightmap.o vector3.o -L"C:\Program Files\Common Files\MinGW\freeglut\lib" -lfreeglut -lopengl32 -lglu32
