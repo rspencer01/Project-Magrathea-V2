c75acc6beb7270a1a21678bbba3a695f44476571
Robert Spencer <robert.spencer94@gmail.com>
2013-08-22 07:53:17 +0200

    More headers, a change of ignore and removal of docs

---
+++ src/include/gameops.h
@@ -0,0 +1,36 @@
+/**
+ * @file gameops.h
+ * @author Robert Spencer <robert.spencer94@gmail.com>
+ * @section DESCRIPTION
+ *
+ * This file contains the gameops class, that handles keypresses and rendering.
+ */
+
+#ifndef GAMEOPS_H
+#define GAMEOPS_H
+
+#include <camera.h>
+#include <region.h>
+
+/// The gameops class contains all the methods to do with gameplay
+///
+/// This class interacts with glut graphics and keyboard handlers, and contains all the objects assiciated with a game instance.
+class gameops
+{
+  private:
+    /// A list of all keys that are currently depressed
+    bool keyDown [256];
+    /// Event handler for a key press
+    void keyPress(unsigned char key, int, int);
+    /// Event handler for a key release
+    void keyUp(unsigned char key, int, int);
+    /// Called on each frame update.  Performs relevant operations, based on key positions
+    void keyOperations();
+    /// Called each frame to show stuff on the screen
+    void display();
+  public:
+    /// Creates a game instance and initialises all variables
+    gameops();
+}
+
+#endif
