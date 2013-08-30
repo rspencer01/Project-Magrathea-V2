699b357b81100ded1599bde74359e12242feffe5
Robert Spencer <robert.spencer94@gmail.com>
2013-08-30 08:13:05 +0200

    Reinstated all rendering

--- src/game.cpp
+++ src/game.cpp
@@ -190,7 +190,6 @@ void Game::constructRegions(float x,float y)
 		Region* rg = new Region(rx,ry,this);
 		regions[0].push_back(rg);
 	}
-  return;
   
 	if (regions.back().back()->getOriginY() < ry+REGION_SIZE)
 	{
