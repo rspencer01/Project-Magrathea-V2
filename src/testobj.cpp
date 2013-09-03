46a1f458131af5ed5ef17fc9f24d8891895cdb25
Robert Spencer <robert.spencer94@gmail.com>
2013-09-03 20:40:56 +0200

    Reinstated colours

--- src/testobj.cpp
+++ src/testobj.cpp
@@ -13,15 +13,15 @@ TestObj::TestObj(Game* parent) : Object(Vector3(0,0,0),parent)
   addTriangle(2,0,1,3);
   addTriangle(3,1,2,3);
   addPoint(4,Vector3(-10,-10,-5),Vector3(1,0,1),0,0,1);
-  addPoint(5,Vector3(-10,10,-5),Vector3(1,0,1),0,0,1);
-  addPoint(6,Vector3(10,-10,-5),Vector3(1,0,1),0,0,1);
+  addPoint(5,Vector3(-10,10,-5),Vector3(1,0,1),0,1,1);
+  addPoint(6,Vector3(10,-10,-5),Vector3(1,0,1),1,1,1);
   addPoint(7,Vector3(10,10,-5),Vector3(1,0,1),0,0,1);
   addTriangle(4,4,5,6);
   addTriangle(5,7,5,6);
 
-  addPoint(8,Vector3(-3,0,3),Vector3(1,0,1),0,0,1);
-  addPoint(9,Vector3(-1,0,3),Vector3(1,0,1),0,0,1);
-  addPoint(10,Vector3(-2,2,3),Vector3(1,0,1),0,0,1);
+  addPoint(8,Vector3(-3,0,3),Vector3(1,0,1),1,0,1);
+  addPoint(9,Vector3(-1,0,3),Vector3(1,0,1),0,1,1);
+  addPoint(10,Vector3(-2,2,3),Vector3(1,0,1),1,1,1);
   addTriangle(6,8,9,10);
   
 
