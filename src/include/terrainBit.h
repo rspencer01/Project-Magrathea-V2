330a3ae8658454c2348d749eabbac703fa54179b
Robert Spencer <robert.spencer94@gmail.com>
2013-09-04 22:56:37 +0200

    Code cleanup in terrainBit

--- src/include/terrainBit.h
+++ src/include/terrainBit.h
@@ -4,12 +4,15 @@
 #include <vector3.h>
 
 /// The terrain bit is a representation of stuff at some point
-typedef 
-struct
+typedef struct
 {
+  /// The position of this piece of ground (typically 1m lateral resolution)
   Vector3* position;
+  /// The normal here
   Vector3* normal;
+  /// Is this point a tree?
   bool isTree;
+  /// Is this point a grass piece?
   bool isGrass;
 } terrainBit;
 
