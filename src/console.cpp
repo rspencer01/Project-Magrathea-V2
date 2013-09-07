d27ad17bc1923316283e459f275585fb3384a3d9
Robert Spencer <robert.spencer94@gmail.com>
2013-09-07 16:31:26 +0200

    Topological maps and better map handling

--- src/console.cpp
+++ src/console.cpp
@@ -1,8 +1,70 @@
 #include <stdio.h>
 #include <game.h>
 
-void runConsole(Game* game)
+char map[100][100];
+Game* gam;
+
+void printMap()
 {
+  printf("+");
+  for (int i = 0;i<100;i++)
+    printf("-");
+  printf("+\n");
+  for (int i = 0;i<100;i++)
+  {
+    printf("|");
+    for (int j = 0;j<100;j++)
+      printf("%c",map[i][j]);
+    printf("|\n");
+  }
+  printf("+");
+  for (int i = 0;i<100;i++)
+    printf("-");
+  printf("+\n");
+}
+
+void clearMap()
+{
+  for (int i = 0;i<100;i++)
+    for (int j = 0;j<100;j++)
+      map[i][j]=' ';
+}
+
+void doVeg()
+{
+  for (int y = 0;y<100;y++)
+    for (int x = 0;x<100;x++)
+    {
+      terrainBit here = gam->getTerrainBit(x,y);
+      if (here.isTree)
+        map[y][x] = 'T';
+    }
+}
+
+void doContour()
+{
+  for (int y = 1;y<100;y++)
+  {
+    for (int x = 1;x<100;x++)
+    {
+      bool isContour = false;
+      if ((int)(gam->getTerrainBit(x,y).position->y/5) > (int)(gam->getTerrainBit(x-1,y).position->y/5))
+        isContour = true;
+      if ((int)(gam->getTerrainBit(x,y).position->y/5) > (int)(gam->getTerrainBit(x+1,y).position->y/5))
+        isContour = true;
+      if ((int)(gam->getTerrainBit(x,y).position->y/5) > (int)(gam->getTerrainBit(x,y-1).position->y/5))
+        isContour = true;
+      if ((int)(gam->getTerrainBit(x,y).position->y/5) > (int)(gam->getTerrainBit(x,y+1).position->y/5))
+        isContour = true;
+      if (isContour) 
+        map[y][x] = '.';
+    }
+  }
+}
+
+void runConsole(Game* gme)
+{
+  gam = gme;
   printf("Entering console mode...\n");
   bool showMenu = true;
   while (1)
@@ -13,29 +75,26 @@ void runConsole(Game* game)
       printf("------------\n");
       printf("Show/Hide (m)enu\n");
       printf("Display (v)egitation\n");
+      printf("Display (c)ontours\n");
+      printf("Display (t)opological (composite)\n");
       printf("(q)uit\n");
     }
-    char option;
-    scanf("%c",&option);
+    clearMap();
+    char option = getchar();
     if (option=='m')
       showMenu = !showMenu;
     if (option=='q')
       break;
     if (option=='v')
+      doVeg();
+    if (option=='c')
+      doContour();
+    if (option=='t')
     {
-      for (int y = 0;y<80;y++)
-      {
-        for (int x = 0;x<80;x++)
-        {
-          terrainBit here = game->getTerrainBit(x,y);
-          if (here.isTree)
-            printf("T");
-          else
-            printf(" ");
-        }
-        printf("\n");
-      }
+      doContour();
+      doVeg();
     }
-
+    printMap();
+    getchar();
   }
 }
