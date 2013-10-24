0e0e8f7aac395c7a30988e4eade2d728dfaea032
Robert Spencer <robert.spencer94@gmail.com>
2013-10-24 10:41:56 +0200

    Corrected sky glitch

--- src/sky.cpp
+++ src/sky.cpp
@@ -17,7 +17,11 @@ Sky::Sky(Game* parent) : Object(Vector3(0,0,0),parent)
              1.f,1.f,1.f);
     editTextureCoord(y,0.5+0.5*sin(-y/10.0*3.1415),0.5+0.5*cos(-y/10.0*3.1415));
 	}
-	addPoint(21,Vector3(0.f,1000.f,0.f),Vector3(0,1,0),1.f,1.f,1.f);
+	addPoint(21,Vector3(1000.f*(float)sin(21/10.f*3.1415),
+						  1000.f,
+						  1000.f*(float)cos(21/10.f*3.1415)),
+              Vector3(0,1,0),
+              1.f,1.f,1.f);
   editTextureCoord(21,0.5,0.5);
 	
 	for (int y = 0; y<20;y++)
