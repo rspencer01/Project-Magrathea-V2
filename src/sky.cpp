9f20bf2987745b22ba061b4aba1c9b4a68fbdf5f
Robert Spencer <robert.spencer94@gmail.com>
2013-10-26 20:45:50 +0200

    Code cleanup

--- src/sky.cpp
+++ src/sky.cpp
@@ -5,42 +5,17 @@
 
 Sky::Sky(Game* parent) : Object(Vector3(0,0,0),parent)
 {
-  textureNumber = textureFromTGA("../assets/sky.tga",true);
-  clearTriangleData(22,21);
-	for (int y = 0; y<21;y++)
-	{
-		addPoint(y,
-             Vector3(1000.f*(float)sin(y/10.f*3.1415),
-						 -100.f,
-						 1000.f*(float)cos(y/10.f*3.1415)),
-             Vector3(0,1,0),
-             1.f,1.f,1.f);
-    editTextureCoord(y,0.5+0.5*sin(-y/10.0*3.1415),0.5+0.5*cos(-y/10.0*3.1415));
-	}
-	addPoint(21,Vector3(1000.f*(float)sin(21/10.f*3.1415),
-						  2000.f,
-						  1000.f*(float)cos(21/10.f*3.1415)),
-              Vector3(0,1,0),
-              1.f,1.f,1.f);
-  editTextureCoord(21,0.5,0.5);
-	
-	for (int y = 0; y<20;y++)
-		addTriangle(y,y,(y+1),21);
-
-  // And save
-	pushTriangleData();
-
+  // The sky has no geometry.  This is just here in case it will in some future point.
+  // Should probably also handle clouds here.  That will come later
   sun = new Sun(Vector3(0,0,0),parent);
 }
 
 void Sky::Render(int refreshTime, Vector3* cameraPos)
 {
+  //Make sure that the sun is ALWAYS behind everything, by disabling depth testing
   glDepthMask(false);
   sun->Render(refreshTime,cameraPos);
   glDepthMask(true);
-  game->currentShader->setFloat("sunIntensity",1.0);
-  //Object::Render(refreshTime,cameraPos);
-  
 }
 
 Sun::Sun(Vector3 pos,Game* parent) : Object(pos,parent)
@@ -77,7 +52,7 @@ void Sun::Render(int refreshTime,Vector3* cameraPos)
          Vector3(sin(theta),cos(theta),0));
   updateTriangleData();
   // We want to be fully lit.
-  
+  game->currentShader->setFloat("sunIntensity",1.0);
   Object::Render(refreshTime,cameraPos);
   // Then light the rest as per usual.
   if (cos(theta)>0)
