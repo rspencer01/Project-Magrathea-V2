c125acbfd22235d2ab0a0d102e9343f6ee5251ae
Robert Spencer <robert.spencer94@gmail.com>
2014-01-16 14:26:01 +0200

    Bigger water size

--- src/water.cpp
+++ src/water.cpp
@@ -6,33 +6,35 @@
 
 Water::Water(glm::vec3 pos,Game* parent) : Object(pos,parent)
 {
+  int waterSize = 600;
+  int detail = 1;
   textureNumber = textureFromTGA("../assets/water.tga",true);
-  clearTriangleData((50+1)*(50+1),50*50*2);
+  clearTriangleData((waterSize+1)*(waterSize+1),waterSize*waterSize*2);
 	// There are (size+1)^2 vertices.  Bring in the data
-	for (int y = 0; y<(50+1);y++)
-		for (int x = 0; x<(50+1);x++)
+	for (int y = 0; y<(waterSize+1);y++)
+		for (int x = 0; x<(waterSize+1);x++)
 		{
-      addPoint(y*(50+1)+x,
-               glm::vec3((float)x,
+      addPoint(y*(waterSize+1)+x,
+               glm::vec3((float)x*detail,
                        0,
-			                 (float)y),
+			                 (float)y*detail),
                        glm::vec3(0,1,0),
                        1,1,1);
-      editTextureCoord(y*(50+1)+x,4.f*x/(50+1),4.f*y/(50+1));
+      editTextureCoord(y*(waterSize+1)+x,4.f*x/(waterSize+1),4.f*y/(waterSize+1));
 		}
 	
   // Populate one triangle (for now) per block
-	for (int y = 0; y<50;y++)
-		for (int x = 0; x<50;x++)
+	for (int y = 0; y<waterSize;y++)
+		for (int x = 0; x<waterSize;x++)
 		{
-      addTriangle((y*50+x)*2,
-                  y*(50+1) + x,
-			            (y+1)*(50+1) + x,
-			            y*(50+1) + (x+1));
-      addTriangle((y*50+x)*2+1,
-                  y*(50+1) + x+1,
-			            (y+1)*(50+1) + x,
-			            (y+1)*(50+1) + (x+1));
+      addTriangle((y*waterSize+x)*2,
+                  y*(waterSize+1) + x,
+			            (y+1)*(waterSize+1) + x,
+			            y*(waterSize+1) + (x+1));
+      addTriangle((y*waterSize+x)*2+1,
+                  y*(waterSize+1) + x+1,
+			            (y+1)*(waterSize+1) + x,
+			            (y+1)*(waterSize+1) + (x+1));
 		}
   pushTriangleData();
   objectData.shinyness = 0.05;
