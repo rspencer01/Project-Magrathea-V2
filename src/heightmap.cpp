3a3ffd755db539a98b2770d5f450d647f6fc8e71
Robert Spencer <robert.spencer94@gmail.com>
2014-01-17 14:09:35 +0200

    Cleaning heightmap

--- src/heightmap.cpp
+++ src/heightmap.cpp
@@ -1,28 +1,16 @@
-#include <stdio.h>
 #include <magrathea.h>
 #include <heightmap.h>
-#include <gtc\noise.hpp>
+#include <gtx\noise.hpp>
 
-FILE* fp;
-
+/// Nothing is done here.  In the future this may include some preprocessing
 void initialiseHeightmap()
 {
-  printf("Initialising heightmap\n");
-  // Load the file (for fast accessing
-  fp = fopen("../assets/smalldata.dat","rb");
-  if (fp==NULL)
-    DIE("Heightmap data not found");
 }
 
+/// Returns a nice multi octave perlin noise
 float getHeightmapData(int x, int y)
 {
   return 50.0*glm::perlin(0.005f*glm::vec2((float)x,(float)y))
      + 5.0*glm::perlin(0.05f*glm::vec2((float)x,(float)y));
-  /*if (fp==NULL)
-    return 0.f;
-  fseek(fp,(x+y*1000)*4,SEEK_SET);
-  char raw[4];
-  fread(raw,4,1,fp);
-  return *((float*)raw)/3-350;*/
 }
 
