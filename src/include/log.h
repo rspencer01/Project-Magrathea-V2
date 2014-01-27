4c78b2732d00b425c4839a7077a4db5d244ab7ea
Robert Spencer <robert.spencer94@gmail.com>
2014-01-27 17:04:28 +0200

    Loggers get windows colours

--- src/include/log.h
+++ src/include/log.h
@@ -4,15 +4,29 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 class Logger
 {
   private:
     FILE* out;
     void beginLog();
+#ifdef _WIN32
+    HANDLE hConsole;
+    WORD colourAttrib;
+#endif
   public:
-    Logger(FILE*);
+    Logger(FILE*,int);
     void log(const char*,...);
 };
+
+#ifdef _WIN32
+#define LOG_COLOUR_RED FOREGROUND_RED
+#define LOG_COLOUR_YELLOW FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY
+#define LOG_COLOUR_GREEN FOREGROUND_GREEN
+#endif
 
 extern Logger loge;
 extern Logger logw;
--- src/log.cpp
+++ src/log.cpp
@@ -2,22 +2,43 @@
 #include <log.h>
 #include <time.h>
 
-Logger loge (stdout);
-Logger logw (stdout);
-Logger logi (stdout);
 
-Logger::Logger(FILE* o)
+Logger loge (stdout,LOG_COLOUR_RED);
+Logger logw (stdout,LOG_COLOUR_YELLOW);
+Logger logi (stdout,LOG_COLOUR_GREEN);
+
+Logger::Logger(FILE* o,int c)
 {
   out = o;
+#ifdef _WIN32
+  if (o==stderr)
+  {
+    hConsole = GetStdHandle(STD_ERROR_HANDLE);
+    colourAttrib = c;
+  }
+  else
+  {
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    colourAttrib = c;
+  }
+#endif
 }
 
 void Logger::beginLog()
 {
+#ifdef _WIN32
+  SetConsoleTextAttribute(hConsole,colourAttrib);
+#endif
+
   char buffer [100];
   time_t rawtime;
   time(&rawtime);
   strftime(buffer,100,"%H:%M:%S",localtime(&rawtime));
   fprintf(out,"[%s] ",buffer);
+
+#ifdef _WIN32
+  SetConsoleTextAttribute(hConsole,FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+#endif
 }
 
 void Logger::log(const char* format, ...)
